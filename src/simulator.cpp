5ce1f59dbbb1e80937e0f248d923f9c9faad07a9
Michael Walton <spesialstyrker@gmail.com>
2014-02-01 19:38:33 -0800

    Refactored source file parsing with address pointer.

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -7,9 +7,15 @@
 #include <vector>
 
 /*
- * Create data structures to simulate PDP-11 memory and registers
+ * Create data structures to simulate PDP-11 memory and registers.
+ * Addresses between 0 and 400 are reserved for interrupt and trap vectors.
+ * Addresses between 400 and 157777 is for processor stack and programs/data.
+ * Addresses above 160000 are reserved for peripheral device status, control, and data regs.
+ *
+ * System is word (16-bit) or byte (8-bit) addressable.
+ * 32768 total words (or 65536) total bytes are available.
  */
-int RAM[177777];
+short RAM[32768];
 
 /*
  * Logistical data structures used by the simulator
@@ -17,6 +23,9 @@ int RAM[177777];
 std::vector<std::string> *instruction;
 std::vector<std::string> *source;
 std::fstream *macFile;
+
+int printbinchar(char);
+int addressPointer;
 
 // Change so we just call the simulator with the .ascii file as the argument for it to parse and run
 int main(int argc, char **argv)
@@ -63,32 +72,48 @@ int main(int argc, char **argv)
     return 0;
   }
 
-  // Perform verification before performing simulator logic
-  
   // First remove the last item in the vector that contains only a '\n' character
   source->pop_back();
+
+  // How do we figure out relative vs absolute addressing mode?
 
   // Make sure each line starts with a - or @ and only has numbers following
   for (std::vector<std::string>::iterator it = source->begin(); it != source->end(); ++it)
   {
-    // Quit if the first character is unrecognized as an address load, store, or PC set
-    if (it->c_str()[0] != '@' && it->c_str()[0] != '*' && it->c_str()[0] != '-')
+    switch(it->c_str()[0])
     {
-      std::cout << "Unrecognized instruction at line " << it - source->begin() << std::endl;
-      return 0;
-    }
+      case '@': // This is the line we set our address pointer to
+        {
+          // Shift addressPointer left 3 bits, then add the next octal value
+          for (std::string::iterator i = (it->begin() + 1); i != it->end(); ++i)
+          {
+            addressPointer = 0;
+            addressPointer = addressPointer << 3;
+            addressPointer = addressPointer + (*i - '0'); // Convert the ascii number to it's integer equivalent.
+          }
 
-    // Quit if a non-numeric character is encountered after the first character
-    else
-    {
-      for (std::string::iterator i = it->begin() + 1; i != it->end(); ++i)
-      {
-        if (!std::isdigit(*i))
+          break;
+        }
+
+      case '-': // This means we load this value in to the memory location pointed to by addressPointer
+        {
+          // Use address pointer to write to mem, then increment address pointer.
+          // If the value loaded in is 16 bits then incremenent address pointer by 2.
+
+          break;
+        }
+
+      case '*': // This is the value we set the PC to
+        {
+
+          break;
+        }
+
+      default:
         {
           std::cout << "Unrecognized instruction at line " << it - source->begin() << std::endl;
-          return 0;
+          //return 0;
         }
-      }
     }
   }
 
@@ -98,18 +123,15 @@ int main(int argc, char **argv)
    * simulator will error out with line number and instruction detail.
    */
 
-  // Begin the loop for instruction break down.
-  for (std::vector<std::string>::iterator it = source->begin(); it != source->end(); ++it)
-  {
-  }
-
   // Run the simulator using the instruction vector
 
   // Just to verify that the Macro11 source file(s) have been parsed correctly.
-  for (std::vector<std::string>::iterator it = source->begin(); it != source->end(); ++it)
-  {
-    std::cout << it->c_str() << std::endl;
-  }
+  //for (std::vector<std::string>::iterator it = source->begin(); it != source->end(); ++it)
+  //{
+    //std::cout << it->c_str() << std::endl;
+  //}
+  
+  std::cout << addressPointer << std::endl;
 
   // Garbage collection
   if (instruction)
